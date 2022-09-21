1ab694bfc5e3521bfb1e6c3fbcb0d99e50d63438
Ayush-pandey-u <113597722+Ayush-pandey-u@users.noreply.github.com>
2022-09-21 05:58:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['919006765023']
-global.OwnerNumber = ['919006765023']
-global.ownertag = ['919006765023']
+global.Owner = ['919073603479']
+global.OwnerNumber = ['919073603479']
+global.ownertag = ['919073603479']
 global.BotName = "chiku"
 global.packname = "chiku-bot"
-global.author = "By: Ayush"
-global.OwnerName = "Ayush"
+global.author = "By: Rajkumar"
+global.OwnerName = "Rajkumar"
 global.BotSourceCode = "https://github.com/Ayush-pandey-u/Chiku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/DYp7FwfL9xsKURihcdT0l8"
 global.sessionName = "session"
